a9a5e9902be79d951ab8a05c20eca492e70a8ba7
tong <tong@disktree.net>
2021-02-24 12:44:18 +0100

    Use top level function

--- src/laerm/Build.hx
+++ src/laerm/Build.hx
@@ -6,10 +6,7 @@ import haxe.macro.Expr;
 import sys.io.File;
 #end
 
-class Build {
-
-    macro public static function getSemver() : ExprOf<String> {
-        var pkg = Json.parse( File.getContent( 'package.json' ) );
-        return macro $v{pkg.version};
-    }
+macro function getSemver() : ExprOf<String> {
+    var pkg = Json.parse( File.getContent( 'package.json' ) );
+    return macro $v{pkg.version};
 }

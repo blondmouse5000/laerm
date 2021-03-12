d0305477038b6d27080779e626ea1666359ee41b
tong <tong@disktree.net>
2021-03-12 15:46:45 +0100

    Use css theme for spectrum colors

--- src/laerm/App.hx
+++ src/laerm/App.hx
@@ -4,13 +4,28 @@ import js.Browser.console;
 import js.Browser.document;
 import js.Browser.window;
 
+typedef Theme = {
+    var background : String;
+    var f_med : String;
+    var b_med : String;
+}
+
 inline var BORDER_WIDTH = 8;
+
+var theme(default,null) : Theme;
 
 function main() {
 
     console.info('%c⸸ LAUTER LAERM ⸸', 'background:#fff000;color:#050505;padding:4px;font-size:23px;');
 
     window.addEventListener( 'load', e -> {
+        
+        var style = window.getComputedStyle( document.documentElement );
+        theme = {
+            background: style.getPropertyValue('--background'),
+            f_med: style.getPropertyValue('--f_med'),
+            b_med: style.getPropertyValue('--b_med')
+        };
 
         var radio = new Radio( "https://rrr.disktree.net:8443", "laerm", "server_version-json.xsl" );
        
--- src/laerm/Spectrum2D.hx
+++ src/laerm/Spectrum2D.hx
@@ -6,65 +6,71 @@ import laerm.App.BORDER_WIDTH;
 
 class Spectrum2D extends Spectrum {
     
-    public var colorStroke = "#050505";
-	public var colorFill = "#fff000";
-
     var graphics : CanvasRenderingContext2D;
     
     public function new( radio : Radio ) {
 		super( radio );
         graphics = radio.canvas.getContext("2d");
-		graphics.strokeStyle = colorStroke;
-		graphics.fillStyle = colorFill;
     }
 
     public function render( timeData : Uint8Array ) {
 
-        graphics.clearRect( 0, 0, radio.canvas.width, radio.canvas.height );
-		
+		var canvas = radio.canvas;
+		var frequencyBinCount = radio.analyser.frequencyBinCount;
 		var v : Float, x : Float, y : Float;
-		var hw = radio.canvas.width/2, hh = radio.canvas.height/2;
+		var hw = canvas.width/2, hh = canvas.height/2;
+       
+		graphics.clearRect( 0, 0, canvas.width, canvas.height );
 		
-		graphics.fillStyle = colorFill;
-		graphics.strokeStyle = colorStroke;
+		graphics.fillStyle = App.theme.b_med;
+		graphics.strokeStyle = App.theme.f_med;
+		graphics.lineWidth = Std.int( (radio.volume.volume*1000) );
 
-		graphics.lineWidth = Std.int( (radio.volume.volume*10)*100 );
+		//drawTime1( 0, 0, canvas.width, canvas.height );
+
+		graphics.lineWidth = Std.int( (radio.volume.volume*1000) );
 		// var c = Std.int(100-radio.volume.rms*255);
 		// graphics.strokeStyle = 'rgba($c,$c,0,1.0)';
 		graphics.beginPath();
 		for( i in 0...radio.analyser.fftSize ) {
-			v = (Math.PI/2)/180*i;
-			x = Math.cos(v) * (timeData[i] );
-			y = Math.sin(v) * (timeData[i] );
+			v = i * (Math.PI/2)/180;
+			x = Math.cos(v) * ( 100+ timeData[i] * (radio.volume.volume*2) );
+			y = Math.sin(v) * ( 100+ timeData[i] * (radio.volume.volume*2) );
 			graphics.lineTo( hw + x, hh + y );
 		}
 		graphics.stroke(); 
- 
-		var width = 24;
-		var height = 24;
-		var ox = BORDER_WIDTH;
-		var oy = BORDER_WIDTH;
-		//var oy = radio.canvas.height - height - 8;
+		
+		var w = 32;
+		var h = 32;
+		var px = BORDER_WIDTH; //canvas.width - (w+BORDER_WIDTH);
+		var py = BORDER_WIDTH; //canvas.height - (h+BORDER_WIDTH);
+		graphics.fillStyle = App.theme.b_med;
+		graphics.strokeStyle = App.theme.f_med;
 		graphics.lineWidth = 1;
-		graphics.fillRect( ox, oy, width, height );
-		graphics.rect( ox, oy, width, height );
+		graphics.fillRect( px, py, w, h );
+		graphics.rect( px, py, w, h );
 		graphics.stroke();
-		graphics.beginPath();
-		var sliceWidth = width * 1.0 / radio.analyser.frequencyBinCount; //(radio.analyser.fftSize/2);
-  		x = 0.0;
-		for( i in 0...radio.analyser.fftSize ) {
-            v = timeData[i] / 128.0;
-			y = v * height / 2;
-			if (i == 0) {
-				graphics.moveTo( ox+x, oy+y);
-			} else {
-				graphics.lineTo( ox+x, oy+y);
-			}
-			x += sliceWidth;
-		}
-		graphics.moveTo(radio.canvas.width, radio.canvas.height / 2);
-		graphics.stroke();
+		drawTime1( px, py, w, h );
     }
+
+	function drawTime1( x : Int, y : Int, w : Int, h : Int ) {
+		var sliceWidth = w * 1.0 / radio.analyser.frequencyBinCount;
+		var cy = h / 2;
+		var px = 0.0, py = 0.0;
+		graphics.beginPath();
+		for( i in 0...radio.analyser.frequencyBinCount ) {
+			var v = radio.timeData[i] / 128.0;
+			py = v * cy;
+			if( i == 0 ) {
+				graphics.moveTo(px+x, py+y);
+			} else {
+				graphics.lineTo(px+x, py+y);
+			}
+			px += sliceWidth;
+		}
+		//graphics.lineTo( w, centerY);
+		graphics.stroke();
+	}
 
 
 }
